52367a9dfdd536dd959cad2fc55a97b7f0220cc5
James Yan <jamesy2027@student.cis.edu.hk>
2025-01-22 12:36:23 +0800

    hyd

--- src/CubeTester.java
+++ src/CubeTester.java
@@ -4,6 +4,35 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CubeTester {
+    public class Cube {
+        private int side;
+
+        public Cube(){
+        this.side=1; }
+
+        public Cube(int side){
+            if(side<=0){
+                throw new IllegalArgumentException("die");
+            }
+            this.side = side;
+        }
+        public int getSide(){
+            return side;
+        }
+        public void setSide(int side) {
+            if (side <= 0) {
+                throw new IllegalArgumentException("dont die");
+            }
+            this.side = side;
+        }
+        public int calculateVolume(){
+            return side*side*side;
+        }
+        public int calculateSurfaceArea(){
+            return 6*side*side;
+        }
+
+    }
 
     @Test
     public void TestNoArgumentConstructorNoException() {
